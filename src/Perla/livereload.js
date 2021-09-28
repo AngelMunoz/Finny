cbab12d690276179ded482f61157bda06ac59d53
Angel D. Munoz <scythes.lol@gmail.com>
2021-09-27 23:29:13 -0500

    fix missing quotes

--- src/Perla/livereload.js
+++ src/Perla/livereload.js
@@ -5,14 +5,14 @@ function replaceCssContent({ oldName, name, content }) {
     const css = content?.replace(/(?:\\r\\n|\\r|\\n)/g, '\n') || "";
     const findBy = oldName || name;
 
-    const style = document.querySelector(`[filename=${findBy}]`);
+    const style = document.querySelector(`[filename="${findBy}"]`);
     if (!style) {
         console.warn("Unable to find", oldName, name);
         return;
     }
 
     style.innerHTML = css;
-    style.setAttribute("data-filename", name);
+    style.setAttribute("filename", name);
 }
 
 worker.addEventListener("message", function({ data }) {
