4ef13e0b14311d4f917e659391d4b7571a485f96
Angel D. Munoz <scythes.lol@gmail.com>
2021-10-02 21:55:40 -0500

    add tsx sample

---
+++ src/react-tsx/src/App.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/src/logo.svg" className="App-logo" alt="logo" />
+        <p>Hello Perla + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
---
+++ src/react-tsx/src/main.tsx
@@ -0,0 +1,11 @@
+import {StrictMode} from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App.jsx'
+
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+)
