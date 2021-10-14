08a2610a929c961fc8c62eb6b65980a32055b410
Angel D. Munoz <scythes.lol@gmail.com>
2021-10-14 09:16:49 -0500

    add fonts for testing

--- src/App/src/Main.fs
+++ src/App/src/Main.fs
@@ -7,6 +7,7 @@ open Fable.Core.JsInterop
 let registerAll: unit -> unit = importMember "fsharp-components"
 importSideEffects "./lit.js"
 importSideEffects "./styles.css"
+importSideEffects "../assets/fonts/fira_code.css"
 
 registerAll ()
 // Start the app
