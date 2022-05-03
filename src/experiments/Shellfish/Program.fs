295d14cde358f921bc6bef3bf54c5405f8dc80b7
Angel D. Munoz <scythes.lol@gmail.com>
2022-05-03 04:15:12 -0500

    copy types into specific directory

--- src/experiments/Shellfish/Program.fs
+++ src/experiments/Shellfish/Program.fs
@@ -67,7 +67,7 @@ let obtainsPackageMetadata (packages: seq<Source * string * string>) =
 
 let downloadTarFile (packageName: string) (version: string) (dist: NpmDist) =
   async {
-    let dir = Directory.CreateDirectory($"./packages").CreateSubdirectory(packageName).CreateSubdirectory(version)
+    let dir = Directory.CreateDirectory($"./perla-deps").CreateSubdirectory(packageName).CreateSubdirectory(version)
     let! file = dist.tarball.GetStreamAsync() |> Async.AwaitTask
     let tar = TarArchive.CreateInputTarArchive(new GZipInputStream(file), Encoding.UTF8)
     tar.ExtractContents(dir.FullName, true)
@@ -80,7 +80,7 @@ let downloadTarFiles (packages: NpmPackageListing list) =
         | Some file ->
             try
               let! path = downloadTarFile package.name package.version file
-              yield path
+              yield path, package
             with ex ->
               Logger.Logger.log("Failed to decompress a file", ex)
         | None -> ()
@@ -111,6 +111,17 @@ let packages =
       |> Result.requireSome "No packages were found in config"
   }
 
+let copyFile (filepath: string) =
+    let pkgIndex = filepath.IndexOf("package")
+
+    let newPath = filepath.Remove(pkgIndex, "package".Length).Replace("perla-deps", "perla-deps/types")
+    Path.GetDirectoryName newPath
+    |> Directory.CreateDirectory
+    |> ignore
+    try
+      File.Copy(filepath, newPath)
+    with ex ->
+      printfn "%s" ex.Message
 
 taskResult {
   let! packages = packages
@@ -123,6 +134,11 @@ taskResult {
 
   let! downloads = Logger.Logger.spinner("Downloading Packages", downloadTarFiles operations)
   Logger.Logger.log($"Downloaded: %A{downloads}".EscapeMarkup())
+
+  for (path, package) in downloads do
+      Directory.GetFiles(path, "*?.d.ts", EnumerationOptions(RecurseSubdirectories = true))
+      |> Array.Parallel.iter copyFile
+
 }
 |> Async.AwaitTask
 |> Async.Ignore
