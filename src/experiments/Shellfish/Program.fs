f85d2555edfe73e9cfd3a7ed94a88ecffe407520
Angel D. Munoz <scythes.lol@gmail.com>
2022-09-26 15:54:40 -0500

    add project scpecific code

--- src/experiments/Shellfish/Program.fs
+++ src/experiments/Shellfish/Program.fs
@@ -17,6 +17,26 @@ open FsToolkit.ErrorHandling
 open Spectre.Console
 open ICSharpCode.SharpZipLib.Tar
 
+let private parseUrl url =
+  let (|ParseRegex|_|) regex str =
+    let m = Text.RegularExpressions.Regex(regex).Match(str)
+
+    if m.Success then
+      Some(List.tail [ for x in m.Groups -> x.Value ])
+    else
+      None
+
+  match url with
+  | ParseRegex @"https://cdn.skypack.dev/pin/(@?[^@]+)@v([\d.]+)"
+               [ name; version ] -> Some(Source.Skypack, name, version)
+  | ParseRegex @"https://cdn.jsdelivr.net/npm/(@?[^@]+)@([\d.]+)"
+               [ name; version ] -> Some(Source.Jsdelivr, name, version)
+  | ParseRegex @"https://ga.jspm.io/npm:(@?[^@]+)@([\d.]+)" [ name; version ] ->
+    Some(Source.Jspm, name, version)
+  | ParseRegex @"https://unpkg.com/(@?[^@]+)@([\d.]+)" [ name; version ] ->
+    Some(Source.Unpkg, name, version)
+  | _ -> None
+
 [<CLIMutable>]
 type NpmDist =
   { fileCount: int64
