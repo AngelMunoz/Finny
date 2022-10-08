141dd83e650894768249f2fb07d4227991f5c4c5
Angel D. Munoz <scythes.lol@gmail.com>
2022-10-08 00:15:42 -0500

    add Flan Cli

---
+++ src/Flan/Extensions.vb
@@ -0,0 +1,44 @@
+﻿Imports System.Runtime.CompilerServices
+Imports Microsoft.FSharp.Core
+
+<Extension>
+Module Extensions
+
+    <Extension>
+    Public Function ToOption(Of T)(value As T) As FSharpOption(Of T)
+        If value Is Nothing Then
+            Return FSharpOption(Of T).None
+        Else
+            Return FSharpOption(Of T).Some(value)
+        End If
+    End Function
+
+    <Extension>
+    Public Function ToValueOption(Of T)(value As T) As FSharpValueOption(Of T)
+        If value Is Nothing Then
+            Return FSharpValueOption(Of T).None
+        Else
+            Return FSharpValueOption(Of T).Some(value)
+        End If
+    End Function
+
+    <Extension>
+    Public Function TryGetValue(Of T)(value As FSharpOption(Of T), ByRef outValue As T)
+        Try
+            outValue = value.Value
+            Return True
+        Catch ex As NullReferenceException
+            Return False
+        End Try
+    End Function
+    <Extension>
+    Public Function TryGetValue(Of T)(value As FSharpValueOption(Of T), ByRef outValue As T)
+        Try
+            outValue = value.Value
+            Return True
+        Catch ex As InvalidOperationException
+            Return False
+        End Try
+    End Function
+
+End Module
---
+++ src/Flan/Program.vb
@@ -0,0 +1,60 @@
+﻿Imports System
+Imports System.CommandLine
+
+Imports Flan.Types
+Imports Logger = Perla.Logger.Logger
+
+Module CliHandlers
+    Friend Async Function HandleAddCommand(package As String, Map As String, Output As String) As Task(Of Integer)
+        Dim _map As String = Nothing
+        Dim _output As String = Nothing
+
+        If Not Map.ToValueOption().TryGetValue(_map) Then
+            _map = "./map.importmap"
+        End If
+
+        If Not Output.ToValueOption().TryGetValue(_output) Then
+            _output = "./map.importmap"
+        End If
+
+        Dim options As New AddPackageOptions With {
+            .Package = package,
+            .Map = _map,
+            .Output = _output
+        }
+        Try
+            Await AddPackage(options)
+            Return 0
+        Catch ex As Exception
+            Logger.log("Unable to add Package", ex)
+            Return 1
+        End Try
+    End Function
+End Module
+
+Module CliOptions
+    Public ReadOnly Property Package As New [Option](Of String)("--package", "name of the package") With {
+        .IsRequired = True
+    }
+    Public ReadOnly Property Map As New [Option](Of String)("--map", "An optional path to an existing import map") With {
+        .IsRequired = False
+    }
+    Public ReadOnly Property Output As New [Option](Of String)("--output", "Path to where to output the new import map") With {
+        .IsRequired = False
+    }
+
+    Public ReadOnly Property AddCommand As New Command("add", "adds a new package to the import map") From {
+        Package, Map, Output
+    }
+End Module
+
+Module Program
+    Sub Main(args As String())
+        AddCommand.SetHandler(AddressOf HandleAddCommand, Package, Map, Output)
+        Dim rootCommand As New RootCommand("This is a Package Manager") From {
+            AddCommand
+        }
+        Dim exitCode = rootCommand.InvokeAsync(args)
+        Environment.Exit(exitCode.GetAwaiter().GetResult())
+    End Sub
+End Module
