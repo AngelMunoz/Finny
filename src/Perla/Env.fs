76f239f78b89d50e34884a6987a731b6dd558f24
Angel D. Munoz <scythes.lol@gmail.com>
2022-10-11 15:14:41 -0500

    move env to its own file

---
+++ src/Perla/Env.fs
@@ -0,0 +1,59 @@
+[<RequireQualifiedAccess>]
+module Perla.Env
+
+open System
+open System.Collections
+open System.Runtime.InteropServices
+open System.Text
+
+open FsToolkit.ErrorHandling
+
+let IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+
+let PlatformString =
+  if RuntimeInformation.IsOSPlatform(OSPlatform.Windows) then
+    "windows"
+  else if RuntimeInformation.IsOSPlatform(OSPlatform.Linux) then
+    "linux"
+  else if RuntimeInformation.IsOSPlatform(OSPlatform.OSX) then
+    "darwin"
+  else if RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD) then
+    "freebsd"
+  else
+    failwith "Unsupported OS"
+
+let ArchString =
+  match RuntimeInformation.OSArchitecture with
+  | Architecture.Arm -> "arm"
+  | Architecture.Arm64 -> "arm64"
+  | Architecture.X64 -> "64"
+  | Architecture.X86 -> "32"
+  | _ -> failwith "Unsupported Architecture"
+
+let getPerlaEnvVars () =
+  let env = Environment.GetEnvironmentVariables()
+  let prefix = "PERLA_"
+
+  [ for entry in env do
+      let entry = entry :?> DictionaryEntry
+      let key = entry.Key :?> string
+      let value = entry.Value :?> string
+
+      if key.StartsWith(prefix) then
+        (key.Replace(prefix, String.Empty), value) ]
+
+let GetEnvContent () =
+  option {
+    let env = getPerlaEnvVars ()
+    let sb = StringBuilder()
+
+    for key, value in env do
+      sb.Append($"""export const {key} = "{value}";""") |> ignore
+
+    let content = sb.ToString()
+
+    if String.IsNullOrWhiteSpace content then
+      return! None
+    else
+      return content
+  }
