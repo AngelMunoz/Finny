e54a40c9187cb00b5fd94652fd27919dd391f577
Angel D. Munoz <scythes.lol@gmail.com>
2022-10-17 15:11:28 -0500

    add fist test :fire:

--- tests/Perla.Tests/Tests.fs
+++ tests/Perla.Tests/Tests.fs
@@ -2,7 +2,47 @@ module Tests
 
 open System
 open Xunit
+open Xunit.Sdk
+
+
+type Env() =
+  do
+    Environment.SetEnvironmentVariable("PERLA_currentEnv", "tests")
+    Environment.SetEnvironmentVariable("PERLA_IAmSet", "yes")
+    Environment.SetEnvironmentVariable("PERLA-NotAvailable", "not-available")
+
+    Environment.SetEnvironmentVariable(
+      "OtherNotAvailable",
+      "other-not-available"
+    )
+
+  [<Fact>]
+  member _.``GetEnvContent provides EnvVars with "PERLA_" prefix``() =
+    let actual = Perla.Env.GetEnvContent()
+
+    match actual with
+    | Some actual ->
+      let expected = "export const IAmSet = \"yes\";"
+      Assert.True(actual.Contains(expected))
+      let expected = "export const currentEnv = \"tests\";"
+      Assert.True(actual.Contains(expected))
+    | None ->
+      raise (XunitException("Content is Empty when It should have data"))
+
+  [<Fact>]
+  member _.``GetEnvContent doesn't provide EnvVars without "PERLA_" prefix``() =
+    let actual = Perla.Env.GetEnvContent()
+
+    match actual with
+    | Some actual ->
+      let expected = "export const NotAvailable = \"not-available\";"
+      Assert.False(actual.Contains(expected))
+      let expected = "export const OtherNotAvailable = \"not-available\";"
+      Assert.False(actual.Contains(expected))
+    | None ->
+      raise (XunitException("Content is Empty when It should have data"))
+
+
 
 [<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+let ``My test`` () = Assert.True(true)
