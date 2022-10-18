73b7ddc96244927406ec42ae8b42f3a3848d14c1
Angel D. Munoz <scythes.lol@gmail.com>
2022-10-17 19:24:17 -0500

    add a couple of json tests

---
+++ tests/Perla.Tests/Json.fs
@@ -0,0 +1,213 @@
+﻿module Json
+
+open System
+open Xunit
+open Xunit.Sdk
+open Perla.Json
+open Thoth.Json.Net
+open FSharp.UMX
+open Perla.Types
+open Perla.Units
+open Perla.PackageManager.Types
+open System.Collections.Generic
+open System.Diagnostics
+
+[<Fact>]
+let ``PerlaDecoder Should Decode from an empty object`` () =
+  match Decode.fromString ConfigDecoders.PerlaDecoder "{}" with
+  | Ok decoded ->
+
+    Assert.True decoded.index.IsNone
+    Assert.True decoded.runConfiguration.IsNone
+    Assert.True decoded.provider.IsNone
+    Assert.True decoded.build.IsNone
+    Assert.True decoded.devServer.IsNone
+    Assert.True decoded.fable.IsNone
+    Assert.True decoded.esbuild.IsNone
+    Assert.True decoded.mountDirectories.IsNone
+    Assert.True decoded.enableEnv.IsNone
+    Assert.True decoded.envPath.IsNone
+    Assert.True decoded.dependencies.IsNone
+    Assert.True decoded.devDependencies.IsNone
+
+  | Error err -> Assert.Fail $"Decoder couldn't decode due: {err}"
+
+[<Fact>]
+let ``PerlaDecoder Should Decode from a complete object`` () =
+  let json =
+    """
+{
+  "index": "index.html",
+  "runConfiguration": "dev",
+  "provider": "jspm",
+  "mountDirectories": {
+    "/src": "./src"
+  },
+  "enableEnv": true,
+  "envPath": "/my-path/env.js",
+  "dependencies": [],
+  "devDependencies": []
+}
+"""
+
+  match Decode.fromString ConfigDecoders.PerlaDecoder json with
+  | Ok decoded ->
+    match decoded.index with
+    | None -> Assert.Fail "index should have a value"
+    | Some index -> Assert.Equal("index.html", UMX.untag index)
+
+    match decoded.runConfiguration with
+    | None -> Assert.Fail "runConfiguration should have a value"
+    | Some runConfiguration ->
+      Assert.Equal(RunConfiguration.Development, runConfiguration)
+
+    match decoded.provider with
+    | None -> Assert.Fail "provider should have a value"
+    | Some provider -> Assert.Equal(Provider.Jspm, provider)
+
+    match decoded.mountDirectories with
+    | None -> Assert.Fail "mountDirectories should have a value"
+    | Some mountDirectories ->
+      Assert.False(mountDirectories |> Map.isEmpty)
+
+      mountDirectories
+      |> Map.tryFindKey (fun k _ -> k = (UMX.tag "/src"))
+      |> Option.isSome
+      |> Assert.True
+
+    match decoded.enableEnv with
+    | None -> Assert.Fail "enableEnv should have a value"
+    | Some enableEnv -> Assert.True enableEnv
+
+    match decoded.enableEnv with
+    | None -> Assert.Fail "enableEnv should have a value"
+    | Some enableEnv -> Assert.True enableEnv
+
+    match decoded.envPath with
+    | None -> Assert.Fail "envPath should have a value"
+    | Some envPath -> Assert.Equal("/my-path/env.js", envPath)
+
+    match decoded.dependencies with
+    | None -> Assert.Fail "dependencies should have a value"
+    | Some dependencies -> Assert.True(dependencies |> Seq.isEmpty)
+
+    match decoded.devDependencies with
+    | None -> Assert.Fail "devDependencies should have a value"
+    | Some devDependencies -> Assert.True(devDependencies |> Seq.isEmpty)
+
+  | Error err -> Assert.Fail $"Decoder couldn't decode due: {err}"
+
+[<Fact>]
+let ``PerlaDecoder Should Decode Fable options`` () =
+  let json =
+    """
+{ "fable": {
+    "project": "./src/App.fsproj",
+    "extension": ".jsx",
+    "sourceMaps": false,
+    "outDir": "../fable-bin"
+  }
+}
+"""
+
+  match Decode.fromString ConfigDecoders.PerlaDecoder json with
+  | Ok { fable = Some fable } ->
+    Assert.Equal(
+      "./src/App.fsproj",
+      fable.project |> Option.map UMX.untag |> Option.defaultValue "bad value"
+    )
+
+    Assert.Equal(
+      ".jsx",
+      fable.extension |> Option.map UMX.untag |> Option.defaultValue "bad value"
+    )
+
+    Assert.False(fable.sourceMaps |> Option.defaultValue true)
+
+    Assert.Equal(
+      "../fable-bin",
+      fable.outDir |> Option.map UMX.untag |> Option.defaultValue "bad value"
+    )
+  | Ok { fable = None } ->
+    Assert.Fail $"Fable is empty when it must have a value"
+  | Error err -> Assert.Fail $"Decoder couldn't decode due: {err}"
+
+
+[<Fact>]
+let ``PerlaDecoder Should Decode DevServer options`` () =
+  let json =
+    """
+{ "devServer": {
+    "port": 5000,
+    "host": "localhost",
+    "liveReload": false,
+    "useSSL": false,
+    "proxy": {
+      "/v1/api/{**catch-all}": "http://localhost:5000/api"
+    }
+  }
+}
+"""
+
+  match Decode.fromString ConfigDecoders.PerlaDecoder json with
+  | Ok { devServer = Some devServer } ->
+    Assert.Equal(
+      5000,
+      devServer.port |> Option.map UMX.untag |> Option.defaultValue 0000
+    )
+
+    Assert.Equal(
+      "localhost",
+      devServer.host |> Option.map UMX.untag |> Option.defaultValue "bad value"
+    )
+
+    Assert.False(devServer.liveReload |> Option.defaultValue true)
+    Assert.False(devServer.useSSL |> Option.defaultValue true)
+
+    devServer.proxy
+    |> Option.map (fun map ->
+      map
+      |> Map.tryFindKey (fun k _ -> k = "/v1/api/{**catch-all}")
+      |> Option.isSome
+      |> Assert.True)
+    |> Option.defaultWith (fun _ ->
+      Assert.Fail "Proxy Should have at least one key")
+  | Ok { devServer = None } ->
+    Assert.Fail $"DevServer is empty when it must have a value"
+  | Error err -> Assert.Fail $"Decoder couldn't decode due: {err}"
+
+
+[<Fact>]
+let ``PerlaDecoder Should Decode Build options`` () =
+  let json =
+    """
+{ "build": {
+    "includes": ["docs/images/**/*.png"],
+    "excludes": [],
+    "outDir": "../dist",
+    "emitEnvFile": true
+  }
+}
+"""
+
+  match Decode.fromString ConfigDecoders.PerlaDecoder json with
+  | Ok { build = Some build } ->
+    match build.includes with
+    | None -> Assert.Fail "Includes should have a value"
+    | Some includes ->
+      let glob = Assert.Single includes
+      Assert.Equal("docs/images/**/*.png", glob)
+
+    match build.excludes with
+    | None -> Assert.Fail "dependencies should have a value"
+    | Some excludes -> Assert.True(excludes |> Seq.isEmpty)
+
+    Assert.Equal(
+      "../dist",
+      build.outDir |> Option.map UMX.untag |> Option.defaultValue "bad value"
+    )
+
+    Assert.True(build.emitEnvFile |> Option.defaultValue false)
+  | Ok { build = None } ->
+    Assert.Fail $"DevServer is empty when it must have a value"
+  | Error err -> Assert.Fail $"Decoder couldn't decode due: {err}"
