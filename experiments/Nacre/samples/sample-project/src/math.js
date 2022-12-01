80474f911a384ab2f0529a7b22a2737909a52bf3
Angel D. Munoz <scythes.lol@gmail.com>
2022-12-01 00:00:24 -0600

    remove nacre last files

--- experiments/Nacre/samples/sample-project/src/math.js
+++
@@ -1,6 +0,0 @@
-export function add(a, b) {
-    if (typeof a !== 'number' || typeof b !== 'number') {
-        throw new TypeError("Both parameters must be numbers");
-    }
-    return (+a) + (+b);
-}
