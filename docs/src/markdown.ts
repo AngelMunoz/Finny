03323c3617ad3f13af445f515ded5cc2670ad8a6
Angel D. Munoz <scythes.lol@gmail.com>
2022-12-01 20:58:12 -0600

    update tsx/jsx projects

--- docs/src/markdown.ts
+++ docs/src/markdown.ts
@@ -1,41 +1,41 @@
-import { marked } from 'marked';
+import { marked } from "marked";
 //@ts-ignore
-import hljs from 'highlight.js/lib/core.js';
+import hljs from "highlight.js/lib/core";
 //@ts-ignore
-import javascript from 'highlight.js/lib/languages/javascript.js';
+import javascript from "highlight.js/lib/languages/javascript";
 //@ts-ignore
-import text from 'highlight.js/lib/languages/plaintext.js';
+import text from "highlight.js/lib/languages/plaintext";
 //@ts-ignore
-import fsharp from 'highlight.js/lib/languages/fsharp.js';
+import fsharp from "highlight.js/lib/languages/fsharp";
 //@ts-ignore
-import bash from 'highlight.js/lib/languages/bash.js';
+import bash from "highlight.js/lib/languages/bash";
 //@ts-ignore
-import json from 'highlight.js/lib/languages/json.js';
+import json from "highlight.js/lib/languages/json";
 //@ts-ignore
-import xml from 'highlight.js/lib/languages/xml.js';
+import xml from "highlight.js/lib/languages/xml";
 
-hljs.registerLanguage('', text);
-hljs.registerLanguage('javascript', javascript);
-hljs.registerLanguage('fsharp', fsharp);
-hljs.registerLanguage('bash', bash);
-hljs.registerLanguage('json', json);
-hljs.registerLanguage('html', xml);
+hljs.registerLanguage("", text);
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("fsharp", fsharp);
+hljs.registerLanguage("bash", bash);
+hljs.registerLanguage("json", json);
+hljs.registerLanguage("html", xml);
 
 marked.setOptions({
-    smartLists: true,
-    smartypants: true,
-    headerIds: true,
-    langPrefix: 'hljs language-',
-    highlight(code, language) {
-        return hljs.highlight(code, { language }).value;
-    }
+  smartLists: true,
+  smartypants: true,
+  headerIds: true,
+  langPrefix: "hljs language-",
+  highlight(code, language) {
+    return hljs.highlight(code, { language }).value;
+  },
 });
 
 export async function fetchMarkdown(url: string) {
-    const res = await fetch(url);
-    if (res.ok) {
-        const content = await res.text();
-        return marked.parse(content);
-    }
-    return Promise.reject(`${res.status} - ${res.statusText}`);
+  const res = await fetch(url);
+  if (res.ok) {
+    const content = await res.text();
+    return marked.parse(content);
+  }
+  return Promise.reject(`${res.status} - ${res.statusText}`);
 }
