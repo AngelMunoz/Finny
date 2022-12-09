0d194260645e15cc4809ea8907a8b30dee238d47
Angel D. Munoz <scythes.lol@gmail.com>
2022-12-08 22:26:38 -0600

    - ensure devServer options are set and respected from the config file - add preliminary "configuration" tests

---
+++ tests/Perla.Tests/Configuration.fs
@@ -0,0 +1,47 @@
+namespace Perla.Tests
+
+open Xunit
+
+open Perla
+open Perla.Types
+open Perla.Logger
+open Perla.Configuration
+
+open FsToolkit.ErrorHandling
+
+module Configuration =
+
+  // we're not picking up env variables yet for perla config
+  [<Fact>]
+  let ``fromEnv returns the same config passed`` () =
+    let config = Defaults.PerlaConfig
+    Assert.Equal(config, fromEnv config)
+
+  [<Fact>]
+  let ``fromFile should update devServer options in perla config`` () =
+    let config = Defaults.PerlaConfig
+
+    let configText =
+      """{
+  "devServer": {
+    "port": 3000,
+    "host": "0.0.0.0",
+    // skip "liveReload" just to ensure the defaults are respected
+    "useSSL": false,
+    "proxy": { "/reqUrl": "https://target" }
+  }
+}"""
+
+    let parsedConfig = Json.getConfigDocument configText
+    let result = Configuration.fromFile (Some parsedConfig) config
+
+    Assert.Equal(
+      result,
+      { config with
+          devServer =
+            { port = 3000
+              host = "0.0.0.0"
+              liveReload = true
+              useSSL = false
+              proxy = Map.ofList [ "/reqUrl", "https://target" ] } }
+    )
