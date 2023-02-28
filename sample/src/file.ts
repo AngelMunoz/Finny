87f1a06353efdfbd46502dbedd052efe4c154ba9
Angel D. Munoz <scythes.lol@gmail.com>
2023-02-28 09:55:46 -0600

    bump

--- sample/src/file.ts
+++ sample/src/file.ts
@@ -1,22 +1,33 @@
-import { html } from 'lit';
+import { html } from "lit";
+import { until } from "lit/directives/until.js";
 // if it doesn't exist it will throw an error
-import { envValue, SOME_TOKEN } from '/env.js'
+// import { envValue, SOME_TOKEN } from '/env.js'
 
-// if you import dinamically it will be undefined
-import("/env.js").then(({ I_DONT_EXIST }) => {
-    console.log(I_DONT_EXIST ?? "export not present")
-})
-
-export function TsSample(value: number, kind: Kind) {
-    return html`
-        <div>
-            <h1 style="color: var(--primary-color)">Hello From Typescript!</h1>
-            <p style="color: var(--primary-color)">The path is ${location.pathname}</p>
-            <p style="color: var(--link-color)">Vaue: ${value}, Kind: ${kind}</p>
-            <p style="color: var(--danger-color)">Env Vars! <b>${envValue}</b>, <b>${SOME_TOKEN}</b> </p>
-        </div>
-    `;
+async function lazyComponent() {
+  try {
+    var { envValue, SOME_TOKEN } = await import("/env.js");
+  } catch (error) {
+    envValue = "not provided";
+    SOME_TOKEN = "not provided";
+  }
+  return html`
+    <p style="color: var(--danger-color)">
+      Env Vars! <b>${envValue}</b>, <b>${SOME_TOKEN}</b>
+    </p>
+  `;
 }
 
+export function TsSample(value: number, kind: Kind) {
+  return html`
+    <div>
+      <h1 style="color: var(--primary-color)">Hello From Typescript!</h1>
+      <p style="color: var(--primary-color)">
+        The path is ${location.pathname}
+      </p>
+      <p style="color: var(--link-color)">Vaue: ${value}, Kind: ${kind}</p>
+      ${until(lazyComponent())}
+    </div>
+  `;
+}
 
-export type Kind = 'a' | 'b' | 'c';
+export type Kind = "a" | "b" | "c";
